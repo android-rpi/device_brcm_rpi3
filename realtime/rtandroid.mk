d7266f889a515611eae2ff04b0321d1df35ec1a4
Igor Kalkov <kalkov@embedded.rwth-aachen.de>
2016-08-18 13:26:08 +0200

    Moved scripts to own folder

--- realtime/rtandroid.mk
+++ realtime/rtandroid.mk
@@ -2,8 +2,8 @@
 $(call inherit-product, vendor/realtime/system/config/common.mk)
 
 # RTAndroid boot animation
-#PRODUCT_COPY_FILES += \
-#    device/brcm/rpi3/realtime/boot.zip:system/media/bootanimation.zip
+PRODUCT_COPY_FILES += \
+    device/brcm/rpi3/realtime/boot.zip:system/media/bootanimation.zip
 
 # Device-specific init scripts
 PRODUCT_COPY_FILES += \

cd99e506b954b0952d66e59b88c8d77cabbfd73b
Igor Kalkov <kalkov@embedded.rwth-aachen.de>
2016-08-19 20:02:02 +0200

    No ignore_edid, 2GB for system partition, script headers

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
 #
+# Copyright (C) 2016 RTAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
 # Android image installation script for Raspberry Pi 3
 # Author: Igor Kalkov
-# https://github.com/RTAndroid/android_device_brcm_rpi3/blob/aosp-n/install.sh
+# https://github.com/RTAndroid/android_device_brcm_rpi3/blob/aosp-n/scripts/install.sh
 #
 
 # global program variables

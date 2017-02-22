cde519303c1aeb0b1c2e6fa990fc2fb773840627
Igor Kalkov <ikalkov@users.noreply.github.com>
2017-02-22 14:06:55 +0100

    Remove density values from PRODUCT_AAPT_CONFIG
    AAPT ignores densities in PRODUCT_AAPT_CONFIG. The use of PRODUCT_AAPT_PREF_CONFIG for preferred density is encouraged, as AAPT is able to determine the fallback density to use if a resource of the specified density does not exist.

--- rpi3.mk
+++ rpi3.mk
@@ -7,9 +7,6 @@ PRODUCT_BRAND := Android
 PRODUCT_MODEL := Raspberry Pi 3
 PRODUCT_MANUFACTURER := brcm
 
-PRODUCT_AAPT_CONFIG := normal tvdpi hdpi
-PRODUCT_AAPT_PREF_CONFIG := tvdpi
-
 include frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk
 
 # application packages
@@ -64,5 +61,6 @@ PRODUCT_COPY_FILES := \
     $(PRODUCT_COPY_FILES)
 
 DEVICE_PACKAGE_OVERLAYS := device/brcm/rpi3/overlay
+PRODUCT_AAPT_PREF_CONFIG := tvdpi
 PRODUCT_CHARACTERISTICS := tv
 PRODUCT_LOCALES := en_US,ko_KR,ja_JP,zh_CN,hi_IN,en_GB,de_DE,fr_FR,it_IT,ru_RU,es_ES,pt_PT

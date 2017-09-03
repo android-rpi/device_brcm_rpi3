a8c0e4ecaed30e5c1ad852f983ff5fddaf7f5ee2
Peter Yoon <peyo.hd@gmail.com>
2017-09-03 21:49:34 +0900

    support oreo  - temporarily disable wifi & bt  - use kernel 4.9.46

--- rpi3.mk
+++ rpi3.mk
@@ -9,22 +9,37 @@ PRODUCT_MANUFACTURER := brcm
 
 include frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.drm.mode.force=1280x720 \
+    ro.opengles.version=131072 \
+    ro.sf.lcd_density=213 \
+    wifi.interface=wlan0
+
 # application packages
 PRODUCT_PACKAGES += \
     Launcher2 \
-    LeanbackLauncher \
-    Settings \
-    Browser2
+    Settings
 
 # system packages
 PRODUCT_PACKAGES += \
     libGLES_mesa \
-    gralloc.$(TARGET_PRODUCT) \
-    hwcomposer.$(TARGET_PRODUCT) \
-    audio.primary.$(TARGET_PRODUCT) \
+    gralloc.rpi3 \
+    hwcomposer.rpi3 \
+    audio.primary.rpi3 \
     audio.usb.default \
     wpa_supplicant \
     wpa_supplicant.conf
+
+# hardware/interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.memtrack@1.0-impl
 
 # system configurations
 PRODUCT_COPY_FILES := \
@@ -32,8 +47,7 @@ PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml \
     $(LOCAL_PATH)/rpi3_core_hardware.xml:system/etc/permissions/rpi3_core_hardware.xml \
     $(LOCAL_PATH)/init.rpi3.rc:root/init.rpi3.rc \
     $(LOCAL_PATH)/init.usb.rc:root/init.usb.rc \
@@ -42,8 +56,6 @@ PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/Generic.kl:system/usr/keylayout/Generic.kl \
     $(LOCAL_PATH)/firmware/brcm/brcmfmac43430-sdio.bin:root/lib/firmware/brcm/brcmfmac43430-sdio.bin \
     $(LOCAL_PATH)/firmware/brcm/brcmfmac43430-sdio.txt:root/lib/firmware/brcm/brcmfmac43430-sdio.txt \
-    $(LOCAL_PATH)/firmware/brcm/BCM43430A1.hcd:root/lib/firmware/brcm/BCM43430A1.hcd \
-    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf \
     $(PRODUCT_COPY_FILES)
 
 # media configurations
